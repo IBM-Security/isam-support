15bc66601529b6d0bd4d468d15578884f86edbbe
Jack Yarborough <jcyarbor@us.ibm.com>
2018-06-12 15:29:44 -0500

    Update mfa_for_partner.js

--- config-example/federation/access_policy/mfa_for_partner.js
+++ config-example/federation/access_policy/mfa_for_partner.js
@@ -4,6 +4,31 @@
 	This mapping rule was created to prompt a user once a session to perform MFA.
 	This is intended to be used as a Federation Access Policy and has been tested on the following platforms : 
 		ISAM 9.0.4.0 - IdP initiated
+		
+	Java class reference can be found on the appliance in the following location : 
+	Manage System Settings -> Secure Settings -> File Downloads ->>
+	federation -> doc -> ISAM-javadoc.zip
+	
+	The following need to be replaced with values from your environment : 
+	
+	<authentication-mechanism-urn> - The URN Identifier of the Authentication Mechanism
+		This can be found at 'Secure Access Control -> Policy -> Authentication ->>
+			Mechanisms -> *authmech* -> Modify Authentication Mechanism ->>
+			General -> Identifier'
+			
+	<protocol> - This should either be 'http' or 'https'
+	
+	<your_idp_host> - The hostname of your IdP Federation
+	
+	<aac_junction> - The name of your AAC Junction
+	
+	<authentication_policy_urn> - The URN Identifier of your Authentication Policy
+		This can be found at 'Secure Access Control -> Policy -> Authentication ->>
+			Policies -> *authpolicy* -> Modify Authentication Policy ->>
+			General -> Identifier'
+	
+	<federation_junction> - The name of the junction associated with your ISAM Federation
+	
 */
 
 importClass(Packages.com.ibm.security.access.policy.decision.Decision);
@@ -55,7 +80,7 @@ if(authMechTypeValues != null){
 		//
 		// If the end user has not completed the authentication Mechanism this session make them step up
 		//
-		if(it.next().toString().equals("urn:ibm:security:authentication:asf:mechanism:eula")){
+		if(it.next().toString().equals("<authentication-mechanism-urn>")){
 			needsMFA = false;
 		}
    }
@@ -78,7 +103,7 @@ if (needsMFA){
 	// Be sure to change the 'PolicyId' to match the Authentication Policy you want the end user to complete
 	//
 	
-	handler.setRedirectUri("https://isam9040.example.lab/mga/sps/authsvc?PolicyId=urn:ibm:security:authentication:asf:eula&Target=/identity@ACTION@");
+	handler.setRedirectUri("<protocol>://<your_idp_host>/<aac_junction> - The name of your AAC Junction/sps/authsvc?PolicyId=<authentication_policy_urn>&Target=/<federation_junction>@ACTION@");
 	
 	var decision = Decision.challenge(handler);
 	context.setDecision(decision);
