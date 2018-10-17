b3ebcc82b463668b4bda4d817c01d30ac045e831
Nick Lloyd <nlloyd@us.ibm.com>
2018-10-17 14:53:54 -0500

    Add files via upload

---
+++ appliance/restapi/Monitoring-AnalysisAndDiagnostics/Logs/ManageReverseProxyLogFiles/Exporting_an_instance-specific_log_file.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+[ "$DEBUG" ] && set -x
+[ "$CURL" ]  || CURL="curl -s -k"
+
+appliance_hostname=$1
+username=$2
+password=$3
+instance_id=$4
+file_id=$5
+
+${CURL} -u "$username:$password" -H "Accept: application/json" -X GET "https://{$appliance_hostname}/wga/reverseproxy_logging/instance/{$instance_id}/${file_id}?export" > $file_id
+
+exit $?
