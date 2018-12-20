caa405bf0cb58998f098ce9d89d88aa5264c8740
Nick Lloyd <nlloyd@us.ibm.com>
2018-12-20 11:55:03 -0600

    Create deploy-pending-changes.sh

---
+++ appliance/restapi/bin/deploy-pending-changes.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+[ "$DEBUG" ] && set -x
+[ "$CURL" ]  || CURL="curl -s -k"
+
+appliance_hostname=$1
+username=$2
+password=$3
+
+
+${CURL} -u "$username:$password" -H "Accept: application/json" -X PUT https://${appliance_hostname}/isam/pending_changes
+
+exit $?
