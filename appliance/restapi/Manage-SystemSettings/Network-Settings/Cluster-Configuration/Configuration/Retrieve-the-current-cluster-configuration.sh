aee327f830f677b9e1fa6bc8f2b53a40728753a0
Nick Lloyd <nlloyd@us.ibm.com>
2019-06-01 14:37:38 -0500

    Create Retrieve-the-current-cluster-configuration.sh

---
+++ appliance/restapi/Manage-SystemSettings/Network-Settings/Cluster-Configuration/Configuration/Retrieve-the-current-cluster-configuration.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+[ "$DEBUG" ] && set -x
+[ "$CURL" ]  || CURL="curl -s -k"
+
+appliance_hostname=$1
+username=$2
+password=$3
+
+${CURL} -u "$username:$password" -H "Accept: application/json" -X GET "https://${appliance_hostname}/isam/cluster/v2"
+
+exit $?
