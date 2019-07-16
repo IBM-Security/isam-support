5cfce82e89c84458c463c6947ac9603344104a19
Nick Lloyd <nlloyd@us.ibm.com>
2019-07-16 08:59:39 -0500

    Create secuser-change-password.js

---
+++ config-example/aac/infomap/scim-related/secuser-change-password.js
@@ -0,0 +1,8 @@
+//
+// Update ISAM secUser object.
+//
+// Use the following when ISAM Integration has been enabled for SCIM
+// so that the secPwdLastChanged attribute on the secUser object is updated.
+//
+var patchJson = '{"schemas":["urn:ietf:params:scim:schemas:extension:isam:1.0:PatchOp"],"Operations":[{ "op":"replace","path":"urn:ietf:params:scim:schemas:extension:isam:1.0:User:password","value": "' + password + '"}]}';
+var resp = ScimClient.httpPatch(scimConfig, "/Users/"+ id, patchJson);
