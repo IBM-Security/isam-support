c05b96e6e7ac06b14ab834223a30d162aa0bf9ed
Jack Yarborough <jcyarbor@us.ibm.com>
2019-07-19 13:31:03 -0500

    Added logic to add a credential attribute
    Initial logic

--- config-example/aac/info_map_js/insert_credential_attr.js
+++ config-example/aac/info_map_js/insert_credential_attr.js
@@ -1,1 +1,18 @@
-// Placeholder
+/*
+	File Name : insert_credential_attr.js
+	Author : Jack Yarborough
+	Contact : jcyarbor@us.ibm.com
+	
+	Usage : This InfoMap JavaScript is designed to perform the following : 
+		- Insert a value into the ISAM credential
+		
+	**This JavaScript mapping rule is provided as-is and is supported by the author.
+	** All variable names, attribute names, and logic are provided as an example
+	
+	Please refer to the following documentation for a complete list of classes that are available for use in InfoMap Mapping rules : 
+		- https://www.ibm.com/support/knowledgecenter/en/SSPREK_9.0.7/com.ibm.isam.doc/config/concept/con_otp_customize_mapping_rules_gs_aac.html
+*/
+
+// Insert an attribute called 'sample' that has a value of 'value'
+context.set(Scope.SESSION, "urn:ibm:security:asf:response:token:attributes", "sample", "value");
+success.setValue(true);
