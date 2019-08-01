d9ecaa72d235ce608a135ec823cefc958bb71cf0
Nick Lloyd <nlloyd@us.ibm.com>
2019-08-01 13:58:25 -0500

    Create run-cli-command.sh

---
+++ appliance/restapi/All/run-cli-command.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+[ "$DEBUG" ] && set -x
+[ "$CURL" ]  || CURL="curl -s -k"
+
+appliance_hostname=$1
+username=$2
+password=$3
+command=$4
+
+${CURL} -u "$username:$password" -H "Accept: application/json" -X POST "https://${appliance_hostname}/core/cli" \
+        --data-ascii "{\"command\":\"$command\"}"
+
+exit $?
