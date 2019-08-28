f9fdb4f81e339bbbf1f738f53eb20b3ef706f562
Nick Lloyd <nlloyd@us.ibm.com>
2019-08-28 14:24:48 -0500

    Create Completing-the-First-Steps-Setup.sh

---
+++ appliance/restapi/First-Steps/Completing-the-First-Steps-Setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+[ "$DEBUG" ] && set -x
+
+appliance_hostname=$1
+username=$2
+password=$3
+
+[ "$CURL" ]  || CURL="curl -s -k -u "$username:$password""
+
+${CURL} -H "Accept: application/json" -H "Content-Type: application/json"       \
+                    -X PUT https://${appliance_hostname}/setup_complete
+
+exit $?
