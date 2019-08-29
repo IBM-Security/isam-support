5d0a369f9e313c15d8d9f120e350f81d6bbdb777
Nick Lloyd <nlloyd@us.ibm.com>
2019-08-29 15:06:34 -0500

    Create Deleting-multiple-log-files.sh

---
+++ appliance/restapi/Monitoring-AnalysisAndDiagnostics/Logs/ApplicationLogFiles/Deleting-multiple-log-files.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+[ "$DEBUG" ] && set -x
+
+appliance_hostname=$1
+username=$2
+password=$3
+payload=$4
+
+[ "$CURL" ]  || CURL="curl -s -k -u "$username:password""
+
+${CURL} -u "$username:$password" -H "Accept: application/json" --data-ascii @${payload} -X PUT "https://${appliance_hostname}/isam/application_logs?action=delete"
+
+exit $?
