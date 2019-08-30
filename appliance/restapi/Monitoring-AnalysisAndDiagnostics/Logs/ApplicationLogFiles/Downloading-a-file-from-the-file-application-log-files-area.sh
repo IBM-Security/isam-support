e063eaf6c93ccba7ef54025f00d2c7b064646267
Nick Lloyd <nlloyd@us.ibm.com>
2019-08-30 11:04:07 -0500

    Update Downloading-a-file-from-the-file-application-log-files-area.sh

--- appliance/restapi/Monitoring-AnalysisAndDiagnostics/Logs/ApplicationLogFiles/Downloading-a-file-from-the-file-application-log-files-area.sh
+++ appliance/restapi/Monitoring-AnalysisAndDiagnostics/Logs/ApplicationLogFiles/Downloading-a-file-from-the-file-application-log-files-area.sh
@@ -6,7 +6,7 @@ username=$2
 password=$3
 file=$4
 
-[ "$CURL" ]  || CURL="curl -s -k -u "$username:password""
+[ "$CURL" ]  || CURL="curl -s -k -u "$username:$password""
 
 ${CURL} -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" -X GET "https://${appliance_hostname}/isam/application_logs/$file?type=File"
 
