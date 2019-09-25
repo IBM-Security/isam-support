b98c79d38057e69006ef477129d35451c823493b
Jack Yarborough <jcyarbor@us.ibm.com>
2019-09-25 14:46:29 -0500

    Update processAndStoreMultiValueAttrs.js

--- config-example/aac/oauth_js/oidc/implicit/processAndStoreMultiValueAttrs.js
+++ config-example/aac/oauth_js/oidc/implicit/processAndStoreMultiValueAttrs.js
@@ -1,3 +1,24 @@
+/*
+  Name : processAndStoreMultiValueAttrs.js
+  Author : jcyarbor@us.ibm.com
+  
+  Intended use : 
+    This mapping rule function was created as a method of retrieving and processing multi-valued attributes to be inserted 
+    into the ID Token during an Implicit flow.
+    
+    This function should be invoked at the '/authorize' endpoint.
+    Please update your code appropriately.
+      
+    ====
+    Usage : 
+    
+	if (populate_id_token || save_cred_attrs) {
+...		
+		processAndStoreMultiValueAttrs("AZN_CRED_GROUPS","groups");
+	}
+    
+    You can take the 'function' from this mapping rule and put it at the end of your PreTokenGeneration mapping rule.
+*/
 function processAndStoreMultiValueAttrs(inputAttr, outputAttr) {
 	if(inputAttr != null && inputAttr != ""){
 		var attrToBeStored = stsuu.getAttributeContainer().getAttributeValuesByNameAndType(inputAttr,"urn:ibm:names:ITFIM:5.1:accessmanager");
