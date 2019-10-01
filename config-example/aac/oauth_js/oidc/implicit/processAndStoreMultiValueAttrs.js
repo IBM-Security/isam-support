90fb0169e7e63126e91e1c13c3318ab68ced15be
Jack Yarborough <jcyarbor@us.ibm.com>
2019-10-01 16:38:38 -0500

    Update processAndStoreMultiValueAttrs.js

--- config-example/aac/oauth_js/oidc/implicit/processAndStoreMultiValueAttrs.js
+++ config-example/aac/oauth_js/oidc/implicit/processAndStoreMultiValueAttrs.js
@@ -23,7 +23,7 @@ function processAndStoreMultiValueAttrs(inputAttr, outputAttr) {
 	if(inputAttr != null && inputAttr != ""){
 		var attrToBeStored = stsuu.getAttributeContainer().getAttributeValuesByNameAndType(inputAttr,"urn:ibm:names:ITFIM:5.1:accessmanager");
 		if(attrToBeStored != null && attrToBeStored.length > 0) {
-			stsuu.addAttribute(new Attribute(outputAttr,"urn:ibm:jwt:claim",attrToBeStored));
+			stsuu.addAttribute(new com.tivoli.am.fim.trustserver.sts.uuser.Attribute(outputAttr,"urn:ibm:jwt:claim",attrToBeStored));
 		}
 	}
 }
