9ebf00ee233fda2804f259871d2cdebec34cfbb4
Nick Lloyd <nlloyd@us.ibm.com>
2019-12-18 10:53:05 -0600

    Update misc.js

--- config-example/aac/infomap/snippet/misc.js
+++ config-example/aac/infomap/snippet/misc.js
@@ -1,4 +1,6 @@
 //
-// About credential attributes.
+// Miscellaneous Examples.
+//
+// Credential attributes.
 //
 var domain = context.get(Scope.SESSION, "urn:ibm:security:asf:response:token:attributes", "AZN_CRED_PRINCIPAL_DOMAIN");
