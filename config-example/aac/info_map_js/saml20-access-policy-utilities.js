1e9c7928f24c78120ac2138cd0f74bb5e2daba5e
Jack Yarborough <jcyarbor@us.ibm.com>
2019-12-24 16:53:27 -0700

    Updated to handle multivalue attributes

--- config-example/aac/info_map_js/saml20-access-policy-utilities.js
+++ config-example/aac/info_map_js/saml20-access-policy-utilities.js
@@ -36,12 +36,21 @@ var userJSON = (function() {
               var attributesReturn = {};
               var attributes = user.getAttributes();
 
-              for (var it = attributes.iterator(); it.hasNext();) {
+             for (var it = attributes.iterator(); it.hasNext();) {
                      var attribute = it.next();
                      var attributeName = attribute.getName();
-                     var attributeValue = attribute.getValue();
-
-                     attributesReturn["" + attributeName] = "" + attributeValue;
+					 var attributeValue = [];
+                     var attributeValues = attribute.getValues();
+					 if(attributeValues.size() > 1 || attributeValues.get(0).includes(",")) {
+						for(var attr = attributeValues.iterator(); attr.hasNext();) {
+							var currentAttr = attr.next();
+							attributeValue.push(currentAttr);
+						}
+						attributesReturn["" + attributeName] = attributeValue;
+					 } else {
+						 attributeValue = attributeValues.get(0);
+						 attributesReturn["" + attributeName] = "" + attributeValue;
+					 }
               }
 
               return attributesReturn;
