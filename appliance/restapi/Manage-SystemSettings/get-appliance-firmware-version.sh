a8f051276979e095c2621927a41360e60afbf069
Jack Yarborough <jcyarbor@us.ibm.com>
2020-02-05 16:50:34 -0600

    Update get-appliance-firmware-version.sh

--- appliance/restapi/Manage-SystemSettings/get-appliance-firmware-version.sh
+++ appliance/restapi/Manage-SystemSettings/get-appliance-firmware-version.sh
@@ -1,1 +1,12 @@
-#Placeholder
+#!/bin/sh
+[ "$DEBUG" ] && set -x
+
+appliance_hostname=$1
+username=$2
+password=$3
+
+[ "$CURL" ]  || CURL="curl -s -k -u "$username:$password""
+
+${CURL} -H "Accept: application/json" -H 'Content-Type: application/json' https://${appliance_hostname}/core/sys/versions | JSON.sh 
+
+exit $?
