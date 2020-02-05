1e0bc627485578a41030d9ef10aefdaf390e7fb2
Jack Yarborough <jcyarbor@us.ibm.com>
2020-02-05 15:57:34 -0600

    Update update-existing-entry.sh

--- appliance/restapi/Secure-WebSettings/Manage/Runtime-Component/Manage/Configuration-files/update-existing-entry.sh
+++ appliance/restapi/Secure-WebSettings/Manage/Runtime-Component/Manage/Configuration-files/update-existing-entry.sh
@@ -11,6 +11,6 @@ value=$7
 
 [ "$CURL" ]  || CURL="curl -s -k -u "$username:$password""
 
-${CURL} -H "Accept: application/json" -H 'Content-Type: application/json' -X PUT https://${appliance_hostname}/isam/runtime/${configurationfile}/configuration/stanza/${stanza}/entry_name/${entry} --data-ascii '{"value":"${value}"}'
+${CURL} -H "Accept: application/json" -H 'Content-Type: application/json' -X PUT https://${appliance_hostname}/isam/runtime/${configurationfile}/configuration/stanza/${stanza}/entry_name/${entry} --data-ascii '{"value":"'${value}'"}'
 
 exit $?
