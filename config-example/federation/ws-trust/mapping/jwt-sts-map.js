eaecccae2efaa49426eaef5609e5f3735e58c513
Jack Yarborough <jcyarbor@us.ibm.com>
2020-02-21 16:54:12 -0600

    Update jwt-sts-map.js

--- config-example/federation/ws-trust/mapping/jwt-sts-map.js
+++ config-example/federation/ws-trust/mapping/jwt-sts-map.js
@@ -3,6 +3,7 @@ importPackage(Packages.com.tivoli.am.fim.trustserver.sts.oauth20);
 importPackage(Packages.com.tivoli.am.fim.trustserver.sts.uuser);
 importClass(Packages.com.tivoli.am.fim.trustserver.sts.utilities.IDMappingExtUtils);
 importClass(Packages.com.tivoli.am.fim.trustserver.sts.utilities.OAuthMappingExtUtils);
+importClass(Packages.com.tivoli.am.fim.base64.BASE64Utility);
 
 // get the username to put in the subject
 var sub = stsuu.getPrincipalName();
@@ -10,7 +11,8 @@ var sub = stsuu.getPrincipalName();
 // Get the value of the 'AppliesTo_ServiceName' 
 var serviceName = stsuu.getRequestSecurityToken().getAttributeValueByNameAndType("AppliesTo_ServiceName","http://schemas.xmlsoap.org/ws/2004/08/addressing");
 
-var svcValue = OAuthMappingExtUtils.SHA512Sum(serviceName);
+var svcValue = BASE64Utility.encode(OAuthMappingExtUtils.SHA512Sum(serviceName));
+
 
 stsuu.clearAttributeList();
 
