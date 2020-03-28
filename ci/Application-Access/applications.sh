b1b03ad5c7959b0f475a3e850cca2917636ab0cc
Nick Lloyd <nlloyd@us.ibm.com>
2020-03-28 17:21:06 -0500

    Create applications.sh

---
+++ ci/Application-Access/applications.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+[ "$DEBUG" ] && set -x
+[ "$CURL" ] || CURL="curl -s -k"
+alias jf='python -mjson.tool'
+
+tenant=$1
+access_token=$2
+applicationId=$3
+
+${CURL} -H "Authorization: Bearer ${access_token}" -H "Accept: application/json, text/plain" -X GET "https://${tenant}/v1.0/applications/${applicationId}"
+
+exit $?
