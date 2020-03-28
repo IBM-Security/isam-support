d5ddfa2a3e091812d6c6deb9e787e5ab7c346c29
Nick Lloyd <nlloyd@us.ibm.com>
2020-03-28 11:34:37 -0500

    Update failure-events-details.sh

--- ci/Reports/failure-events-details.sh
+++ ci/Reports/failure-events-details.sh
@@ -7,14 +7,18 @@ alias jf='python -mjson.tool'
 tenant=$1
 access_token=$2
 log=$3
+tmpjson=$$.json
+
+rm -f ${tmpjson}
 
 echo "Pulling Failed Authentication events for ${tenant}"
 
-
 for event in `grep _id ${log} | awk -F\: '{ print $2 }' | awk -F\" '{ print $2 }'`
 do
-    echo $event
-    echo "{\"EVENTID\":\"${event}\"}" > temp.json
-    ./reports.sh ${tenant} ${access_token} auth_event_details temp.json  | jf
-    echo ""
+        echo ${event}
+        echo "{\"EVENTID\":\"${event}\"}" > ${tmpjson}
+        ./reports.sh ${tenant} ${access_token} auth_event_details ${tmpjson} | jf
+        echo ""
 done
+
+rm -f ${tmpjson}
