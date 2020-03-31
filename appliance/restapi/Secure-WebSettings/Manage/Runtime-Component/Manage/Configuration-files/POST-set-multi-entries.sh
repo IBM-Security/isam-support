bd90f5bdb8f0b95b96e0b0b0926236c226a15546
Jack Yarborough <jcyarbor@us.ibm.com>
2020-03-31 09:48:49 -0500

    Add files via upload

---
+++ appliance/restapi/Secure-WebSettings/Manage/Runtime-Component/Manage/Configuration-files/POST-set-multi-entries.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+[ "$DEBUG" ] && set -x
+[ "$CURL" ]  || CURL="curl -s -k"
+
+appliance_hostname=$1
+username=$2
+password=$3
+id=$4
+stanza=$5
+entry=$6
+json=$7
+payload=`UTILITY-create-multi-entry-json.sh ${entry} |tail -1`
+
+echo ${payload}
+if [ -z ${json} ]
+then
+	${CURL} -u "$username:$password" -H "Accept: application/json" -X POST -H "Content-type: application/json" --data ${payload} "https://${appliance_hostname}/wga/reverseproxy/${id}/configuration/stanza/${stanza}/entry_name/${entry}" -vv | JSON.sh
+else
+	${CURL} -u "$username:$password" -H "Accept: application/json" -X POST -H "Content-type: application/json" --data $payload "https://${appliance_hostname}/wga/reverseproxy/${id}/configuration/stanza/${stanza}/entry_name/${entry}" -vv
+fi
+
+exit $?
