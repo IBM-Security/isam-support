071482e95aa99ba2b322fcf3cd5347028f70bd6a
Jack Yarborough <jcyarbor@us.ibm.com>
2020-03-31 10:35:08 -0500

    Add files via upload

---
+++ appliance/restapi/bin/UTILITY_input-csv_output-json_create-multi-entry-json.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+# UTILITY-create-multi-entry-json.sh
+# input a file containing comma separated values and output a single JSON with format : 
+#{"entries"[["entry","value"]]}
+[ "$DEBUG" ] && set -x
+
+payload='{"entries":['
+printf "Starting Payload : %s\n" $payload
+
+entry=$1
+filelist=$2
+file=
+
+if [ -z ${entry} ]
+then
+	printf "No entry supplied\n"
+	exit $?
+fi
+
+if [ -z ${filelist} ]
+then
+	read -p "Set payload file path : " path
+	echo "These files should be a single line that is the value of the entry specified"
+	
+	 read -p "Enter input 'entry' file name : "  file
+	while [ ! -z ${file} ]
+	do
+		if [ ! -z ${file} ]
+		then
+			for value in $(cat ${path}/${file} | sed 's/:/\n/g' )
+			do
+				values='["'${entry}'","'$(cat ${path}/${file})'"],'$values
+				echo $values
+			done
+		fi
+		read -p "Enter input 'entry' file name (leave blank to complete) : "  file
+	done
+fi
+
+payload=${payload}${values%?}']}'
+
+echo "${payload}"
