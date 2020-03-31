39d895b7ec38a9dfd095c21296dadc7257bc7b66
Jack Yarborough <jcyarbor@us.ibm.com>
2020-03-31 10:56:19 -0500

    Update POST-set-multi-entries.sh

--- appliance/restapi/Secure-WebSettings/Manage/Runtime-Component/Manage/Configuration-files/POST-set-multi-entries.sh
+++ appliance/restapi/Secure-WebSettings/Manage/Runtime-Component/Manage/Configuration-files/POST-set-multi-entries.sh
@@ -8,8 +8,24 @@ password=$3
 configurationfile=$4
 stanza=$5
 entry=$6
-json=$7
-payload=`UTILITY-create-multi-entry-json.sh ${entry} |tail -1`
+inputtype=$7
+json=$8
+
+if [ ! -z ${inputtype} ]
+then
+	if [ ${inputtype} = "json" ]
+	then
+		payload=`UTILITY_input-json_output-json_create-multiple-entries.sh ${entry} |tail -1`
+	elif [ ${inputtype} = "csv" ]
+	then
+		payload=`UTILITY_input-csv_output-json_create-multiple-entries.sh ${entry} |tail -1`
+	else
+		printf "Invalid file type specified\n"
+	fi
+else
+	printf "No input file type specified, defaulting to JSON input\n"
+	payload=`UTILITY-create-multi-entry-json.sh ${entry} |tail -1`
+fi
 
 echo ${payload}
 if [ -z ${json} ]
