a928db7d6eb722b95233bfdd32c24d7adb07b4da
Jack Yarborough <jcyarbor@us.ibm.com>
2020-03-31 10:51:07 -0500

    Update POST-set-multi-entries.sh

--- appliance/restapi/Secure-WebSettings/Manage/Runtime-Component/Manage/Configuration-files/POST-set-multi-entries.sh
+++ appliance/restapi/Secure-WebSettings/Manage/Runtime-Component/Manage/Configuration-files/POST-set-multi-entries.sh
@@ -14,7 +14,7 @@ payload=`UTILITY-create-multi-entry-json.sh ${entry} |tail -1`
 echo ${payload}
 if [ -z ${json} ]
 then
-	${CURL} -u "$username:$password" -H "Accept: application/json" -X POST -H "Content-type: application/json" --data ${payload} "https://${appliance_hostname}/isam/runtime/${configurationfile}/configuration/stanza/${stanza}/entry_name/${entry}" -vv | JSON.sh
+	${CURL} -u "$username:$password" -H "Accept: application/json" -X POST -H "Content-type: application/json" --data $payload "https://${appliance_hostname}/isam/runtime/${configurationfile}/configuration/stanza/${stanza}/entry_name/${entry}" -vv | JSON.sh
 else
 	${CURL} -u "$username:$password" -H "Accept: application/json" -X POST -H "Content-type: application/json" --data $payload "https://${appliance_hostname}/isam/runtime/${configurationfile}/configuration/stanza/${stanza}/entry_name/${entry}" -vv
 fi
