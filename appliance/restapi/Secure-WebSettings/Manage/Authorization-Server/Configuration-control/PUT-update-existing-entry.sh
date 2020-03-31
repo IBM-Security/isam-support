d2670aee6b4e4f99d75a25fad9d953e3a24a2de5
Jack Yarborough <jcyarbor@us.ibm.com>
2020-03-31 10:01:03 -0500

    Create PUT-update-existing-entry.sh

---
+++ appliance/restapi/Secure-WebSettings/Manage/Authorization-Server/Configuration-control/PUT-update-existing-entry.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+[ "$DEBUG" ] && set -x
+
+appliance_hostname=$1
+username=$2
+password=$3
+instance=$4
+stanza=$5
+entry=$6
+value=$7
+
+[ "$CURL" ]  || CURL="curl -s -k -u "$username:$password""
+
+${CURL} -H "Accept: application/json" -H 'Content-Type: application/json' -X PUT https://${appliance_hostname}/isam/authzserver/${instance}/configuration/stanza/${stanza}/entry_name/${entry}/v1 --data-ascii '{"value":"'${value}'"}'
+
+exit $?
