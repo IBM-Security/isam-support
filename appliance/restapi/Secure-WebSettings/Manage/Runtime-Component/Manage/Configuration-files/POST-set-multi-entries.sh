7cef1363b2a962cc3fe0b6327a489fed1fb63eb9
Jack Yarborough <jcyarbor@us.ibm.com>
2020-03-31 10:50:41 -0500

    Update POST-set-multi-entries.sh

--- appliance/restapi/Secure-WebSettings/Manage/Runtime-Component/Manage/Configuration-files/POST-set-multi-entries.sh
+++ appliance/restapi/Secure-WebSettings/Manage/Runtime-Component/Manage/Configuration-files/POST-set-multi-entries.sh
@@ -5,7 +5,7 @@
 appliance_hostname=$1
 username=$2
 password=$3
-id=$4
+configurationfile=$4
 stanza=$5
 entry=$6
 json=$7
@@ -14,9 +14,9 @@ payload=`UTILITY-create-multi-entry-json.sh ${entry} |tail -1`
 echo ${payload}
 if [ -z ${json} ]
 then
-	${CURL} -u "$username:$password" -H "Accept: application/json" -X POST -H "Content-type: application/json" --data ${payload} "https://${appliance_hostname}/wga/reverseproxy/${id}/configuration/stanza/${stanza}/entry_name/${entry}" -vv | JSON.sh
+	${CURL} -u "$username:$password" -H "Accept: application/json" -X POST -H "Content-type: application/json" --data ${payload} "https://${appliance_hostname}/isam/runtime/${configurationfile}/configuration/stanza/${stanza}/entry_name/${entry}" -vv | JSON.sh
 else
-	${CURL} -u "$username:$password" -H "Accept: application/json" -X POST -H "Content-type: application/json" --data $payload "https://${appliance_hostname}/wga/reverseproxy/${id}/configuration/stanza/${stanza}/entry_name/${entry}" -vv
+	${CURL} -u "$username:$password" -H "Accept: application/json" -X POST -H "Content-type: application/json" --data $payload "https://${appliance_hostname}/isam/runtime/${configurationfile}/configuration/stanza/${stanza}/entry_name/${entry}" -vv
 fi
 
 exit $?
