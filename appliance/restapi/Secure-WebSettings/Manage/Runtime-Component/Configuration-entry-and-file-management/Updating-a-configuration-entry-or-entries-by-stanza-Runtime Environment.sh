7d9193cfd38bf79c37e453215ee2f324108b56e2
Nick Lloyd <nlloyd@us.ibm.com>
2020-09-21 08:56:35 -0500

    Create Updating-a-configuration-entry-or-entries-by-stanza-Runtime Environment.sh

---
+++ appliance/restapi/Secure-WebSettings/Manage/Runtime-Component/Configuration-entry-and-file-management/Updating-a-configuration-entry-or-entries-by-stanza-Runtime Environment.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+[ "$DEBUG" ] && set -x
+
+appliance_hostname=$1
+username=$2
+password=$3
+configurationfile=$4
+stanza=$5
+entry=$6
+value=$7
+
+[ "$CURL" ]  || CURL="curl -s -k -u "$username:$password""
+
+${CURL} -H "Accept: application/json" -H 'Content-Type: application/json' -X PUT https://${appliance_hostname}/isam/runtime/${configurationfile}/configuration/stanza/${stanza}/entry_name/${entry} --data-ascii '{"value":"'${value}'"}'
+
+exit $?
+
