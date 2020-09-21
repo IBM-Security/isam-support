0e720441fde99127f8bbdfc55ced2a86da50d702
Nick Lloyd <nlloyd@us.ibm.com>
2020-09-21 15:43:46 -0500

    Create enable-pdweb-debug.sh

---
+++ config-example/docker/k8s/enable-pdweb-debug.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+kubectl exec --stdin=true $1  -- isam_cli -v -e -c isam admin<<EOF
+login -a sec_master -p Passw0rd
+s t $1 trace set pdweb.debug 9 file path=pdweb.debug.log
+EOF
+
+exit $?
