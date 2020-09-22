4b74916699a1777965c1882ba928b26296b990d3
Nick Lloyd <nlloyd@us.ibm.com>
2020-09-22 14:21:59 -0500

    Update INFOMAP_COMMON.js

--- config-example/aac/infomap/infomap-debugger/INFOMAP_COMMON.js
+++ config-example/aac/infomap/infomap-debugger/INFOMAP_COMMON.js
@@ -6,19 +6,37 @@ function dumpMap(scope, namespace, name) {
     var theMap = context.get(scope, namespace, name);
 
     if (theMap != null) {
-	var mapKeySet    = theMap.keySet();
-	var numKeys = theMap.size();
-	
-        trace("INFOMAPDEBUGGER:    Map      = ", theMap);
-	trace("INFOMAPDEBUGGER:    Set        = ", mapKeySet);
-	trace("INFOMAPDEBUGGER:    Length = ", numKeys);
+    var mapKeySet    = theMap.keySet();
+    var numKeys = theMap.size();
 
-	var theKeyArray = theMap.keySet().toArray();
-	var i;
-	for(i=0; i < numKeys; i++) {
-	    var aKey = theKeyArray[i]; 
-	    var aValue = theMap.get(aKey);
-	    trace("INFOMAPDEBUGGER:      ", aKey + "=" + aValue[0]);
-	}
+        trace("INFOMAPDEBUGGER:    Map      = ", theMap);
+    trace("INFOMAPDEBUGGER:    Set        = ", mapKeySet);
+    trace("INFOMAPDEBUGGER:    Length = ", numKeys);
+
+    var theKeyArray = theMap.keySet().toArray();
+    var i;
+    for(i=0; i < numKeys; i++) {
+        var aKey = theKeyArray[i];
+        var aValue = theMap.get(aKey);
+        trace("INFOMAPDEBUGGER:      ", aKey + "=" + aValue[0]);
+    }
+    }
+}
+
+function dumpContextArray(contextArray, scope, attrs_type) {
+    var attr_name = "";
+    var attr_value = "";
+    var tag = "";
+
+    trace("INFOMAPDEBUGGER:    Length = ", contextArray.length);
+
+    if (contextArray != null) {
+        for (var i = 0; i < contextArray.length; i++) {
+            attr_name = contextArray[i];
+            attr_value = context.get(scope, attrs_type, attr_name);
+            tag = attr_name + " = " + attr_value
+            trace("INFOMAPDEBUGGER:    ", tag);
+
+        }
     }
 }
