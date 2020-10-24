1a64df31cd91f588f80d240281e96dfe59998aee
Nick Lloyd <nlloyd@us.ibm.com>
2020-10-24 16:27:30 -0500

    ClientCredentials Script

---
+++ config-example/aac/src/oauth/ClientCredentials/oauth-client-credentials.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+[ "$DEBUG" ] && set -x
+
+HOST=$1
+CLIENT_ID=$2
+CLIENT_SECRET=$3
+SCOPE=$4
+
+curl -s -k -u "$CLIENT_ID:$CLIENT_SECRET" -H 'Content-Type: application/x-www-form-urlencoded'	\
+	-H 'Accept: text/html'	\
+	-d "grant_type=client_credentials&client_id=$CLIENT_ID&scope=$SCOPE" "https://$HOST/mga/sps/oauth/oauth20/token"
+
+exit $?
