3f5215aa20e797c95cd01bf168cfc93a364902cd
Nick Lloyd <nlloyd@us.ibm.com>
2020-11-09 19:09:53 -0600

    Update get-authorize.sh

--- config-example/federation/legacy-oidc/get-authorize.sh
+++ config-example/federation/legacy-oidc/get-authorize.sh
@@ -7,7 +7,7 @@ CLIENT_ID=$2
 CLIENT_SECRET=$3
 
 ${CURL} --cookie cookie.jar -X GET		\
-				"https://$HOST/isam/oidc/endpoint/amapp-runtime-LEGACY/authorize?response_type=code&redirect_uri=https://isam9070-web.level2.org/dashboard/&client_id=cid&scope=openid"
+				"https://$HOST/isam/oidc/endpoint/amapp-runtime-LEGACY/authorize?response_type=code&redirect_uri=https://www.redirect.org/dashboard&client_id=cid&scope=openid"
 
 exit $?
 
