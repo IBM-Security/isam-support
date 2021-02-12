1444ec3e6daac3454ee549bc6c44765dc04eeef4
Nick Lloyd <nlloyd@us.ibm.com>
2021-02-12 14:20:06 -0600

    Add files via upload

--- diagnostic/appliance/wga-monitor.sh
+++ diagnostic/appliance/wga-monitor.sh
@@ -7,80 +7,102 @@
 #
 # History:
 #
-# 2020-06-19:       Intitial version.
-#
-# Convert 4096 Bytes to GB: (X * 4) / 1048576
-# Convert 1024 Bytes to GB: X / 1048576
+# 2020-06-19:		Intitial version.
+# 2020-07-13:		Added systemStats and memory.
+# 2020-07-17:		Mimic options for snmpwalk.
+# 2021-02-12:		Added looped for specific MIBs.
 #
 [ "$DEBUG" ] && set -x
 
 refresh_rate=300
+version=2c
+community=public
 
 cleanUp()
 {
-    exit 0
+	exit 0
 }
 
 Usage()
 {
     echo ""
-    echo "Usage: $0 -h appliance_hostname [ -r refresh_rate ]"
+    echo "Usage: $0 -h appliance_hostname [ -r refresh_rate ] [ -v snmp version ] [ -c community ]"
     echo ""
     echo "where:"
     echo "  -h:             Appliance hostname."
     echo "  -r:             The refresh rate.  The default is 300 seconds."
+    echo "  -v:             SNMP version.  1, 2c, or 3.  The default is 2c."
+    echo "  -c:             SNMP Community.  The default is public."
+	echo ""
+	echo "The script runs continously.  Use <Ctrl>C to stop."
 
     exit 1
 }
 
-while getopts h:r: name
+while getopts h:r:v:c: name
 do
-    case $name in
-        h) appliance_hostname="$OPTARG"
-           HOST="$OPTARG"
-           ;;
+	case $name in
+		h) appliance_hostname="$OPTARG"
+		   HOST="$OPTARG"
+		   ;;
 
-        r) refresh_rate="$OPTARG"
-           ;;
+		r) refresh_rate="$OPTARG"
+		   ;;
 
-        *) Usage
-           ;;
-    esac
+		v) version="$OPTARG"
+		   ;;
+
+		c) community="$OPTARG"
+		   ;;
+
+		*) Usage
+		   ;;
+	esac
 done
+
 
 trap cleanUp INT
 
 [ "$HOST" ] || Usage
 
-SNMP_SYSTEM_NAME=`snmpwalk  -v 2c -c public $HOST SNMPv2-MIB::sysName.0 | awk '{ print $4 }'`
+SNMP_SYSTEM_NAME=`snmpwalk -v $version -c $community $HOST SNMPv2-MIB::sysName.0 | awk '{ print $4 }'`
 
 while :
 do
-    TIME_STAMP=`date +"%Y-%m-%d-%H-%M-%S"`
-    OUTPUT_LOG="$TIME_STAMP-$SNMP_SYSTEM_NAME.log"
+	TIME_STAMP=`date +"%Y-%m-%d-%H-%M-%S"`
+	OUTPUT_LOG="$TIME_STAMP-$SNMP_SYSTEM_NAME.log"
 
-    echo "$SNMP_SYSTEM_NAME ($TIME_STAMP)"
-    echo "$SNMP_SYSTEM_NAME ($TIME_STAMP)" > $OUTPUT_LOG
+	echo "$SNMP_SYSTEM_NAME ($OUTPUT_LOG)"
+	echo "$SNMP_SYSTEM_NAME ($TIME_STAMP)" > $OUTPUT_LOG
+	
+	for table in rStorageTable hrSWRunTable hrSWRunPerfTable tcpConnTable udpTable
+	do
+		echo "=================================================="
+		echo "TABLE = $table"
+		echo "=================================================="
+		snmptable -v $version -c $community $HOST $table
+		echo ""
+	done >> $OUTPUT_LOG
 
-    for table in rStorageTable hrSWRunTable hrSWRunPerfTable
-    do
-        echo "=================================================="
-        echo "TABLE = $table"
-        echo "=================================================="
-        snmptable -mALL -v 2c -c public $HOST $table
-        echo ""
-    done >> $OUTPUT_LOG
+	for comp in systemStats memory hrSWRunTable hrSWRunPerfTable
+	do
+		echo "=================================================="
+		echo "COMP = $comp"
+		echo "=================================================="
+		snmpwalk -v $version -c $community $HOST $comp
+		echo ""
+	done >> $OUTPUT_LOG
 
-    for table in hrSWRunTable hrSWRunPerfTable
-    do
-        echo "=================================================="
-        echo "TABLE = $table"
-        echo "=================================================="
-        snmpwalk -v 2c -c public $HOST $table
-        echo ""
-    done >> $OUTPUT_LOG
-
-    sleep $refresh_rate
+	for mib in TCP-MIB
+	do
+		echo "=================================================="
+		echo "mib = $mib"
+		echo "=================================================="
+		snmpwalk -m $mib -v $version -c $community $HOST | grep $mib
+		echo ""
+	done >> $OUTPUT_LOG
+		
+	sleep $refresh_rate
 done
 
 exit 0
