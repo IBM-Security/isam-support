61acd85a878bd00a32331a787df3c3ed4141392a
Jack Yarborough <jcyarbor@us.ibm.com>
2021-03-09 19:01:28 -0600

    Update infomap_authenticate_ulh_last-login-support.js

--- config-example/aac/info_map_js/infomap_authenticate_ulh_last-login-support.js
+++ config-example/aac/info_map_js/infomap_authenticate_ulh_last-login-support.js
@@ -1,8 +1,16 @@
 /*
 *	Name : infomap_authenticate_ulh_last-login-support.js
+*	Author: jcyarbor@us.ibm.com
 *
+*	Intended Usage:
+*		This mapping rule is intended to authenticate against the internal ISAM registry 
+*		on a single node ISAM appliance with the following activations:
+*			IBM Security Verify Access Base Appliance
+*			IBM Security Verify Access Advanced Access Control
 *
+*		The Java Documentation resides at 'Manage System Settings -> Secure Settings -> File Downloads ->> access_control -> doc -> ISAM-javadoc.zip'
 *
+*		Line 33 should be updated to reflect the hostname, port, credentials, bind-dn, and bind-pwd for your own environment.
 */
 importPackage(Packages.com.ibm.security.access.user);
 importPackage(Packages.com.ibm.security.access.server_connections);
