f220bb7f5a2611ede8bd6c3d48b6fc96633cb701
Nick Lloyd <nlloyd@isamdocker.tivlab.austin.ibm.com>
2021-03-21 18:09:43 -0500

    Added v3 options.

--- diagnostic/appliance/wga-monitor.sh
+++ diagnostic/appliance/wga-monitor.sh
@@ -11,12 +11,13 @@
 # 2020-07-13:		Added systemStats and memory.
 # 2020-07-17:		Mimic options for snmpwalk.
 # 2021-02-12:		Added looped for specific MIBs.
+# 2021-03-21:		Mimc v3 options for snmpwalk.
 #
 [ "$DEBUG" ] && set -x
 
-refresh_rate=300
-version=2c
-community=public
+refresh_rate="300"
+version="2c"
+community="public"
 
 cleanUp()
 {
@@ -25,21 +26,29 @@ cleanUp()
 
 Usage()
 {
-    echo ""
-    echo "Usage: $0 -h appliance_hostname [ -r refresh_rate ] [ -v snmp version ] [ -c community ]"
-    echo ""
-    echo "where:"
-    echo "  -h:             Appliance hostname."
-    echo "  -r:             The refresh rate.  The default is 300 seconds."
-    echo "  -v:             SNMP version.  1, 2c, or 3.  The default is 2c."
-    echo "  -c:             SNMP Community.  The default is public."
+	echo ""
+	echo "Usage: $0 -h appliance_hostname [ -r refresh_rate ] [ -v snmp version ] [ -c community ] [ -l security_level ] [ -a protocol ] [-u security name ] [ -A passphrase ] [ -x privacy protcol ] [-X privacy passphrase ]"
+	echo ""
+	echo "where:"
+	echo "  -h:		Appliance hostname."
+	echo "  -r:		The refresh rate.  The default is 300 seconds."
+	echo "  -v:		SNMP version.  1, 2c, or 3.  The default is 2c."
+	echo "  -c:		SNMP Community.  The default is public."
+	echo ""
+	echo "V3 options when using -v 3."
+	echo "  -l:		Security Level.  One of (noAuthNoPriv|authNoPriv|authPriv)"
+	echo "  -a:		Authentication Protocol.  One of (MD5|SHA)"
+	echo "  -u:		Security name used for authenticated SNMPv3 messages."
+	echo "  -A:		Authentication passphrase used for authenticated SNMPv3 messages"
+	echo "  -x:		Privacy protocol (DES or AES) used for encrypted SNMPv3 messages"
+	echo "  -X:		Privacy passphrase used for encrypted SNMPv3 messages."
 	echo ""
 	echo "The script runs continously.  Use <Ctrl>C to stop."
 
     exit 1
 }
 
-while getopts h:r:v:c: name
+while getopts h:r:v:c:l:a:u:A:x:X: name
 do
 	case $name in
 		h) appliance_hostname="$OPTARG"
@@ -55,17 +64,41 @@ do
 		c) community="$OPTARG"
 		   ;;
 
+		l) security_level="$OPTARG"
+		   ;;
+
+		a) authentication_protocol="$OPTARG"
+		   ;;
+
+		u) security_name="$OPTARG"
+		   ;;
+
+		A) authentication_passphrase="$OPTARG"
+		   ;;
+
+		x) privacy_protocol="$OPTARG"
+		   ;;
+
+		X) privacy_passphrase="$OPTARG"
+		   ;;
+
 		*) Usage
 		   ;;
 	esac
 done
 
-
 trap cleanUp INT
 
 [ "$HOST" ] || Usage
 
-SNMP_SYSTEM_NAME=`snmpwalk -v $version -c $community $HOST SNMPv2-MIB::sysName.0 | awk '{ print $4 }'`
+if [ "$version" = "2c" ]
+then
+	CLI_ARGS="-v $version -c $community $HOST"
+else
+	CLI_ARGS="-v $version -c $community -l $security_level -a $authentication_protocol -u $security_name -A $authentication_passphrase -x $privacy_protocol -X $privacy_passphrase $HOST"
+fi
+
+SNMP_SYSTEM_NAME=`snmpwalk $CLI_ARGS SNMPv2-MIB::sysName.0 | awk '{ print $4 }'`
 
 while :
 do
@@ -80,7 +113,7 @@ do
 		echo "=================================================="
 		echo "TABLE = $table"
 		echo "=================================================="
-		snmptable -v $version -c $community $HOST $table
+		snmptable $CLI_ARGS $table
 		echo ""
 	done >> $OUTPUT_LOG
 
@@ -89,16 +122,16 @@ do
 		echo "=================================================="
 		echo "COMP = $comp"
 		echo "=================================================="
-		snmpwalk -v $version -c $community $HOST $comp
+		snmpwalk $CLI_ARGS $comp
 		echo ""
 	done >> $OUTPUT_LOG
 
 	for mib in TCP-MIB
 	do
 		echo "=================================================="
-		echo "mib = $mib"
+		echo "MIB = $mib"
 		echo "=================================================="
-		snmpwalk -m $mib -v $version -c $community $HOST | grep $mib
+		snmpwalk -m $mib $CLI_ARGS  | grep $mib
 		echo ""
 	done >> $OUTPUT_LOG
 		
