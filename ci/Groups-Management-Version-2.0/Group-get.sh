ed9dfd00c0e43d2b83dff825e7fa7808c8ee546e
Nick Lloyd <nlloyd@isamdocker.tivlab.austin.ibm.com>
2021-03-31 15:52:19 -0500

    New group RAPI

---
+++ ci/Groups-Management-Version-2.0/Group-get.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+[ "$DEBUG" ] && set -x
+[ "$CURL" ] || CURL="curl -s -k"
+
+tenant=$1
+access_token=$2
+id="$3"
+
+${CURL} -H "Authorization: Bearer ${access_token}" -H "Accept: application/scim+json" -X GET "https://${tenant}/v2.0/Groups/${id}"
+
+exit $?
