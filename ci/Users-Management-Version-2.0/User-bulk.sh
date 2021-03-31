2b6747f6b24931e0fad515144393e176d530a3e5
Nick Lloyd <nlloyd@isamdocker.tivlab.austin.ibm.com>
2021-03-31 15:50:09 -0500

    More user RAPI.

---
+++ ci/Users-Management-Version-2.0/User-bulk.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+[ "$DEBUG" ] && set -x
+[ "$CURL" ] || CURL="curl -s -k"
+
+tenant=$1
+access_token=$2
+payload="$3"
+
+${CURL} -H "Authorization: Bearer ${access_token}" -H "Content-Type: application/scim+json" -H "Accept: application/scim+json" -X POST "https://${tenant}/v2.0/Bulk" --data-ascii @${payload}
+
+exit $?
