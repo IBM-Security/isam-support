bb4d48ed26e8553df7d1aea9304eef9f30e05d58
Annelise <39342273+AnneliseQ@users.noreply.github.com>
2021-06-10 18:23:47 -0500

    Update JustGetAACTracefiles.sh
    fixed var substitution error

--- appliance/restapi/Monitoring-AnalysisAndDiagnostics/Logs/ApplicationLogFiles/JustGetAACTracefiles.sh
+++ appliance/restapi/Monitoring-AnalysisAndDiagnostics/Logs/ApplicationLogFiles/JustGetAACTracefiles.sh
@@ -9,7 +9,7 @@ password=$3
 [ "$CURL" ]  || CURL="curl -s -k -u "$username:$password""
 
 
-for id in `curl -s -k -u admin:admin -H "Accept:application/json"  -X GET 'https://192.168.10.70/isam/application_logs/access_control/runtime'  | jq  -r '.contents[].name' | grep 'trace.*.log'`
+for id in `${CURL} -H "Accept:application/json"  -X GET 'https://${appliance_hostname}/isam/application_logs/access_control/runtime'  | jq  -r '.contents[].name' | grep 'trace.*.log'`
 do 
    echo $id
    ${CURL} -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" -X GET "https://${appliance_hostname}/isam/application_logs/access_control/runtime/$id?type=File" -o "$id"
