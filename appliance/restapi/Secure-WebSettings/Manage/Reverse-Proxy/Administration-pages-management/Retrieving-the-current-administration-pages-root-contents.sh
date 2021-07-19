5d37e936cb7cf024d8d306af2ee9e0f04a3ffcc5
Nick Lloyd <nlloyd@isamdocker.tivlab.austin.ibm.com>
2021-07-19 14:17:53 -0500

    New RAPI mgmt pages scripts.

---
+++ appliance/restapi/Secure-WebSettings/Manage/Reverse-Proxy/Administration-pages-management/Retrieving-the-current-administration-pages-root-contents.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+[ "$DEBUG" ] && set -x
+[ "$CURL" ]  || CURL="curl -s -k"
+
+appliance_hostname=$1
+username=$2
+password=$3
+instance_id=$4
+path=$5
+
+${CURL} -u "$username:$password" -H "Accept: application/json" -X GET "https://${appliance_hostname}/wga/reverseproxy/${instance_id}/management_root/${path}"
+
+exit $?
