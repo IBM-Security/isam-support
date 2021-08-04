651edd84a75f886d19b2e4e65e35c9af90a3373f
Nick Lloyd <nlloyd@isamdocker.tivlab.austin.ibm.com>
2021-08-04 13:15:39 -0500

    Shutdown

---
+++ appliance/restapi/Manage-SystemSettings/System-Settings/Restart-or-Shut-down/Shut-down-the-appliance.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+[ "$DEBUG" ] && set -x
+
+appliance_hostname=$1
+username=$2
+password=$3
+
+[ "$CURL" ] || CURL="curl -s -k -u "$username:$password""
+
+${CURL} -H "Accept: application/json" -X POST "https://${appliance_hostname}/diagnostics/restart_shutdown/shutdown"
+
+exit $?
