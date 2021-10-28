e9be7b99f928af94ebabcbc1d50d4ac9b4481874
Jack Yarborough <jcyarbor@us.ibm.com>
2021-10-28 18:36:33 -0500

    Fixed missing parenthesis

--- config-example/federation/identity_mapping_rule_snippets/idp_map_cred_attr_to_saml_attribute.js
+++ config-example/federation/identity_mapping_rule_snippets/idp_map_cred_attr_to_saml_attribute.js
@@ -22,7 +22,7 @@ var ldapEmail = stsuu.getAttributeValueByName("emailAddress");
 3) Add that variable to the STSUU with the desired name and type :
 */
 if(ldapEmail != null && ldapEmail != "") {
-   stsuu.addAttribute(new Attribute("uid", "type", ldapEmail);
+   stsuu.addAttribute(new Attribute("uid", "type", ldapEmail));
 } else {
    IDMappingExtUtils.traceString("There was no email attribute in the credential. Please check LDAP for user : "+ stsuu.getPrincipalName());
 }
