dd0e92764551f2bc587ce1abaf3b651a576073a2
Nick Lloyd <nlloyd@us.ibm.com>
2023-01-29 16:01:11 -0600

    Update request_add_header_with_junction_name.lua

--- config-example/webseal/http-transformations/request/request_add_header_with_junction_name.lua
+++ config-example/webseal/http-transformations/request/request_add_header_with_junction_name.lua
@@ -1,5 +1,7 @@
 if not HTTPRequest.containsHeader("iv-junction") then
    uri = HTTPRequest.getURL()
    junction = string.match(uri, "/(.-)/");
-   HTTPRequest.setHeader("iv-junction", junction)
+   if junction then
+      HTTPRequest.setHeader("iv-junction", junction)
+   end
 end
