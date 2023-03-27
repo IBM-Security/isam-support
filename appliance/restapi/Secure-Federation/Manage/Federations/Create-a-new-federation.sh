b6ce9a0ecb832c6358c585c56964055afc1373c3
Nick Lloyd <nlloyd@isva-l2-docker1.fyre.ibm.com>
2023-03-27 07:04:42 -0700

    Create Federation

--- appliance/restapi/Secure-Federation/Manage/Federations/Create-a-new-federation.sh
+++ appliance/restapi/Secure-Federation/Manage/Federations/Create-a-new-federation.sh
@@ -1,1 +1,16 @@
-.placeholder
+#!/bin/sh
+[ "$DEBUG" ] && set -x
+[ "$CURL" ]  || CURL="curl -s -k"
+
+appliance_hostname=$1
+username=$2
+password=$3
+payload=$4
+
+[ "$CURL" ] && CURL="$CURL -u "$username:$password""
+
+${CURL} -H "Accept: application/json" -H "Content-Type: application/json"		\
+		--data-ascii @${payload}													\
+		-X POST "https://${appliance_hostname}/iam/access/v8/federations/"
+
+exit $?
--- appliance/restapi/System-Settings/Network-Settings/All/Retrieving-the-network-configuration.sh
+++ appliance/restapi/System-Settings/Network-Settings/All/Retrieving-the-network-configuration.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 [ "$DEBUG" ] && set -x
+[ "$CURL" ] || CURL="curl -s -k"
 
 appliance_hostname=$1
 username=$2
 password=$3
 
-[ "$CURL" ] || CURL="curl -s -k -u "$username:$password""
+[ "$CURL" ] && CURL="$CURL -u "$username:$password""
 
 ${CURL} -H 'Accept: application/json' -H 'Content-Type: application/json' -X GET https://${appliance_hostname}/net
 
