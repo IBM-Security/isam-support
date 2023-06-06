68c39577001b3193d1f999fe31aba15f5ec188de
Jack Yarborough <jcyarbor@us.ibm.com>
2023-06-06 17:24:54 -0500

    Update saml20-json-rtss-utilities.js
    reverted change from subject back to environment attributes

--- config-example/aac/info_map_js/saml20-json-rtss-utilities.js
+++ config-example/aac/info_map_js/saml20-json-rtss-utilities.js
@@ -160,10 +160,10 @@ function buildDefaultBrowserRiskProfile (JSONBody) {
 	var httpAcceptLanguage = requestJSON.headers["Accept-Language"];
 	var httpUserAgent = requestJSON.headers["User-Agent"];
 	
-	addStringSubjAttr("urn:ibm:security:environment:http:accept",httpAccept,JSONBody);
-	addStringSubjAttr("urn:ibm:security:environment:http:acceptEncoding",httpAcceptEncoding,JSONBody);
-	addStringSubjAttr("urn:ibm:security:environment:http:acceptLanguage",httpAcceptLanguage,JSONBody);
-	addStringSubjAttr("urn:ibm:security:environment:http:userAgent",httpUserAgent,JSONBody);
+	addStringEnvAttr("urn:ibm:security:environment:http:accept",httpAccept,JSONBody);
+	addStringEnvAttr("urn:ibm:security:environment:http:acceptEncoding",httpAcceptEncoding,JSONBody);
+	addStringEnvAttr("urn:ibm:security:environment:http:acceptLanguage",httpAcceptLanguage,JSONBody);
+	addStringEnvAttr("urn:ibm:security:environment:http:userAgent",httpUserAgent,JSONBody);
 	
 	return JSONBody;
 }
