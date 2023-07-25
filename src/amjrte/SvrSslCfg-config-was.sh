f2ec477d540ddadec3fe92970c831e0924db395c
Nick Lloyd <nlloyd@us.ibm.com>
2023-07-25 10:04:40 -0500

    Update SvrSslCfg-config-was.sh

--- src/amjrte/SvrSslCfg-config-was.sh
+++ src/amjrte/SvrSslCfg-config-was.sh
@@ -1,6 +1,21 @@
 #!/bin/sh -x
-
+##
+## Update as needed.
+##
 WAS=/opt/IBM/WebSphere/AppServer
+
+##
+## Make sure the WAS Java is in the path.
+## cd $WAS/bin
+## . ./setupCmdLine.sh
+## which java
+## /opt/IBM/WebSphere/AppServer/java/8.0/bin/java
+##
+
+##
+## NOTE:
+## The -authzsvr argument is required but there does not need to actually be a PDACLD running.
+## The -port and -dbdir arguments are required but a remote mode app does not actually use them.
 
 java -cp $WAS/tivoli/tam/PD.jar -Dpd.cfg.home=$WAS/tivoli/tam com.tivoli.pd.jcfg.SvrSslCfg -action config                 \
                                   -admin_id sec_master -admin_pwd sec_master_password                                     \
