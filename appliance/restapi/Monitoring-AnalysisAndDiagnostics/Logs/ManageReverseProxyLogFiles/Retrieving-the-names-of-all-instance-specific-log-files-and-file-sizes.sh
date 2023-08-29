6a0daf3a50df4c46ef2aaf8328ac23b605956342
Nick Lloyd <nlloyd@us.ibm.com>
2023-08-29 13:26:11 -0500

    Update Retrieving-the-names-of-all-instance-specific-log-files-and-file-sizes.sh

--- appliance/restapi/Monitoring-AnalysisAndDiagnostics/Logs/ManageReverseProxyLogFiles/Retrieving-the-names-of-all-instance-specific-log-files-and-file-sizes.sh
+++ appliance/restapi/Monitoring-AnalysisAndDiagnostics/Logs/ManageReverseProxyLogFiles/Retrieving-the-names-of-all-instance-specific-log-files-and-file-sizes.sh
@@ -8,6 +8,6 @@ password=$3
 instance_id=$4
 
 
-${CURL} -u "$username:$password" -H "Accept: application/json" -X GET "https://{$appliance_hostname}/wga/reverseproxy_logging/instance/{$instance_id}"
+${CURL} -u "$username:$password" -H "Accept: application/json" -X GET "https://${appliance_hostname}/wga/reverseproxy_logging/instance/${instance_id}"
 
 exit $?
