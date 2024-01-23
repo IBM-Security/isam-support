3510ccd0a9a22defaee8aabe559ffd7f60e0916b
Jack Yarborough <jcyarbor@us.ibm.com>
2024-01-23 13:41:09 -0600

    Update timeout.go
    Added references to code/resources used to create file.

--- src/testresources/golang/timeout.go
+++ src/testresources/golang/timeout.go
@@ -1,5 +1,12 @@
 // Author: jcyarbor@us.ibm.com
 // Title: timeout.go
+//
+// Resources and References:
+//	https://gobyexample.com/http-servers
+//	https://stackoverflow.com/questions/17573190/how-to-multiply-duration-by-integer
+//	https://freshman.tech/snippets/go/extract-url-query-params/
+//	https://stackoverflow.com/questions/38511526/check-empty-float-or-integer-value-in-golang
+//	https://pkg.go.dev/strconv#Atoi
 // 
 // Description: A simple HTTP server that has a single endpoint, timeout, that takes an optional query parameter, timeout.
 // 		The script will timeout in seconds equivalent to the query parameter or after 31 seconds by default.
