0ed4567b53ae1e3e2d38459742af5fd20dd9bb0c
prasadtushar <67198118+prasadtushar@users.noreply.github.com>
2024-01-24 15:22:27 +0800

    Add files via upload

--- config-example/webseal/ModSecurity/waf01.lua
+++ config-example/webseal/ModSecurity/waf01.lua
@@ -1,1 +1,15 @@
 
+  --created by Yutaka Kanemoto
+
+
+Control.trace(5, "waf01: called for " .. HTTPRequest.getURL())
+
+local cname="iam"
+if string.match(HTTPRequest.getURL(), "/env.cgi") then
+    if HTTPRequest.containsCookie(cname) and string.match(HTTPRequest.getCookie(cname), "admin") then
+        Control.trace(5, "waf01: you are admin. not triggered!")
+    else
+        Control.trace(5, "waf01: you are NOT admin. Triggered!")
+        Control.triggerWAF("1,2,5")
+    end
+end
