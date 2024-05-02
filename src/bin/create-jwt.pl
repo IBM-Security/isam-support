809469a34e604596d065b3943c029697d1d95e86
Nick Lloyd <nlloyd@isva-l2-kvm1.fyre.ibm.com>
2024-05-02 16:59:13 -0700

    JWT creator

---
+++ src/bin/create-jwt.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Crypt::JWT qw(encode_jwt decode_jwt);
+use Crypt::PK::RSA;
+use Data::Dumper;
+
+########################
+# Encode (Create signature)
+########################
+
+my $payload = {
+        iss => "issuer",
+        sub => "iuser",
+        exp => time + 86400,
+};
+
+my $keyfile = "/home/nlloyd/git/isam-support/src/bin/key.pem";
+my $key = Crypt::PK::RSA->new($keyfile);
+my $alg = "RS256";
+
+my $token = encode_jwt(payload => $payload, key => $key, alg => $alg);
+print "\n";
+print "token: $token\n\n";
+
+########################
+# Decode (Verify signature)
+########################
+
+my $pubkeyfile = "/home/nlloyd/git/isam-support/src/bin/certificate.pem";
+my $pubkey = Crypt::PK::RSA->new($pubkeyfile);
+my $data = decode_jwt(token => $token, key => $pubkey);
+print Dumper $data;
