0022ae5f3adc751dbedea091d90892be1d92a1b0
Jack Yarborough <jcyarbor@us.ibm.com>
2024-09-16 15:32:22 -0500

    Update sp_saml_20_MSFT_Entra_as_IDP.js
    Updated to adjust variable scoping and use Regular Expression that could take into account a trailing slash which would also be an unacceptable character for an HTTP Header name.

--- config-example/federation/identity_mapping_rule_snippets/sp_saml_20_MSFT_Entra_as_IDP.js
+++ config-example/federation/identity_mapping_rule_snippets/sp_saml_20_MSFT_Entra_as_IDP.js
@@ -6,7 +6,21 @@ importClass(Packages.com.tivoli.am.fim.trustserver.sts.utilities.IDMappingExtUti
 importClass(Packages.com.tivoli.am.fim.trustserver.sts.uuser.Attribute);
 importClass(Packages.com.tivoli.am.fim.trustserver.sts.uuser.Group);
 
-var incomingAttributes = stsuu.getAttributeContainer();
+let matchingPatterns = [
+	/^http:\/\/schemas.xmlsoap.org\/ws\/2005\/05\/identity\/claims\/([^\/]+)$/,
+	/^http:\/\/schemas.microsoft.com\/identity\/claims\/([^\/]+)$/
+];
+
+function extractEntraAttributeShortName(s) {
+	let result = null;
+	for (let i = 0; i < matchingPatterns.length && result == null; i++) {
+		let m = s.match(matchingPatterns[i]);
+		result = (m != null && m.length == 2) ? m[1] : null;
+	}
+	return result;
+}
+
+let incomingAttributes = stsuu.getAttributeContainer();
 
 // We are now going to clear the STSUU Attribute List
 stsuu.clearAttributeList()
@@ -17,30 +31,24 @@ stsuu.clearAttributeList()
 //
 // Since we cleared the STSUU Attribute List we need to re-add the attributes
 // We need to change the attribute name format from URI to just a name
-var incomingAttrArray = incomingAttributes.getAttributes();
-var numberOfAttrs = incomingAttributes.getNumberOfAttributes();
+let incomingAttrArray = incomingAttributes.getAttributes();
+let numberOfAttrs = incomingAttributes.getNumberOfAttributes();
 
-for(var i = 0; i < numberOfAttrs; i++) {
-	var currentAttr = incomingAttrArray[i];
+for(let i = 0; i < numberOfAttrs; i++) {
+	let currentAttr = incomingAttrArray[i];
 	
-	var currentAttrName = currentAttr.getName();
+	let currentAttrName = currentAttr.getName();
 	
-	var fixedAttrName = "";
+	let fixedAttrName = "";
 	
 	// Adding this variable as an optional prefix for attributes from Entra
-	var prefix = "";
+	let prefix = "";
 	
 	// Attribute names from Entra can have two types of formats:
 	//    http://schemas.xmlsoap.org/ws/2005/05/identity/claims/<attr>
 	//    http://schemas.microsoft.com/identity/claims/<attr>
 	//
-	//  So what we'll do is detect whether there's a '/' character in the name, split the 'name' into an Array
-	//  at the '/' characters, and then use the last entry of the array to get the name
-	if(currentAttrName.includes("/")) {
-		fixedAttrName = prefix + currentAttrName.split("/").pop()
-	} else {
-		fixedAttrName = prefix + currentAttrName;
-	}
+	fixedAttrName = prefix + extractEntraAttributeShortName(currentAttrName);
 	
 	currentAttr.setName(fixedAttrName);
 	stsuu.addAttribute(currentAttr)
