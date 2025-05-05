2a69165cfeb2d0df0aefc94b94237814776873de
Nick Lloyd <nlloyd@isva-l2-docker1.fyre.ibm.com>
2025-05-05 16:24:03 -0500

    Snapshot

---
+++ appliance/restapi/Manage-SystemSettings/System-Settings/Snapshots/Deleting-a-snapshot.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+[ "$DEBUG" ] && set -x
+
+appliance_hostname=$1
+username=$2
+password=$3
+id=$4
+
+[ "$CURL" ]  || CURL="curl -s -k -u "$username:$password""
+
+${CURL} -H "Accept: */*"		\
+	-H "Accept-Encoding: gzip, deflate, br"								\
+	-X DELETE https://${appliance_hostname}/snapshots/${id}
+
+exit $?
